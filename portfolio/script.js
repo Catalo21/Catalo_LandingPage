219a1dcc597cdb920c39c1188f64f8c12b545ca0
Catalo21 <96164347+Catalo21@users.noreply.github.com>
2023-12-06 19:34:16 +0800

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,39 @@
+// Function to handle smooth scrolling
+function smoothScroll(target, duration) {
+    const targetSection = document.querySelector(target);
+    const targetPosition = targetSection.getBoundingClientRect().top;
+    const startPosition = window.pageYOffset;
+    const distance = targetPosition - startPosition;
+    let startTime = null;
+  
+    function animation(currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+  
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+  
+    requestAnimationFrame(animation);
+  }
+  
+  // Smooth scroll when clicking on navigation links
+  const navLinks = document.querySelectorAll('nav ul li a');
+  
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      const targetSectionId = e.currentTarget.getAttribute('href');
+      smoothScroll(targetSectionId, 1500); // You can adjust the duration here
+    });
+  });
+  
+ 
+
